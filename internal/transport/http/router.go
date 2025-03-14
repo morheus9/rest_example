dc04d925fcabdf6a7f800512b40b3518e9f34ef1
Vyacheslav <nodegopher@gmail.com>
2025-03-14 13:15:43 +0300

    fixed router

--- internal/transport/http/router.go
+++ internal/transport/http/router.go
@@ -3,43 +3,24 @@ package http
 import (
 	"net/http"
 	"strconv"
-	"strings"
 )
 
 func NewRouter(handler *Handler) http.Handler {
 	mux := http.NewServeMux()
 
 	// Регистрация маршрутов
-	mux.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			handler.CreateUser(w, r)
-		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}
+	mux.HandleFunc("POST /users", func(w http.ResponseWriter, r *http.Request) {
+		handler.CreateUser(w, r)
 	})
 
-	mux.HandleFunc("/users/", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			// Извлекаем ID из URL
-			parts := strings.Split(r.URL.Path, "/")
-			if len(parts) < 3 {
-				http.Error(w, "Invalid URL", http.StatusBadRequest)
-				return
-			}
-			idStr := parts[2]
-			id, err := strconv.ParseInt(idStr, 10, 64)
-			if err != nil {
-				http.Error(w, "Invalid ID", http.StatusBadRequest)
-				return
-			}
-
-			// Вызываем GetUser с извлеченным ID
-			handler.GetUser(w, r, id)
-		default:
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	mux.HandleFunc("GET /users/{id}", func(w http.ResponseWriter, r *http.Request) {
+		idStr := r.PathValue("id")
+		id, err := strconv.ParseInt(idStr, 10, 64)
+		if err != nil {
+			http.Error(w, "Invalid ID", http.StatusBadRequest)
+			return
 		}
+		handler.GetUser(w, r, id)
 	})
 
 	return mux
